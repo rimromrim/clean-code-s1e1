10b463b4f59f08c70094a04f22058de725f4a7c1
Semyon <cemen12532@gmail.com>
2023-04-24 22:27:50 +0500

    refactor: changed attribute name to lowercase according to rule 1.2 in html-and-css.md

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
 
 var taskInput = document.getElementById("new-task");//Add a new task.
 var addButton = document.querySelector(".app-task-btn");
-var incompleteTaskHolder = document.getElementById("incompleteTasks");//ul of #incompleteTasks
+var incompleteTaskHolder = document.getElementById("incompletetasks");//ul of #incompleteTasks
 var completedTasksHolder = document.getElementById("completed-tasks");//completed-tasks
 
 
